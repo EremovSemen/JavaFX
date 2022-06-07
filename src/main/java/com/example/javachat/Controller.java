24d9a589f4c460316089acc8264b07d56f3cc07b
EremovSemen <semaeremov@mail.ru>
2022-06-07 17:10:29 +0300

    chat

---
+++ src/main/java/com/example/javachat/Controller.java
@@ -0,0 +1,26 @@
+package com.example.javachat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextArea historyArea;
+    @FXML
+    private TextField userAnswer;
+
+    public void clickCheckButton(ActionEvent actionEvent) {
+    }
+//    @FXML
+//    private Label welcomeText;
+//
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Ты пидор");
+//    }
+}
